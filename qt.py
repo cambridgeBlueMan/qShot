ae3345d96bb45ea66f92d34f26dd3e9235015502
lea nicholson <cambridgeblueman@gmail.com>
2025-11-20 17:04:50 +0000

    compatibility change for pyqt5/6 picamera widgets

--- qt.py
+++ qt.py
@@ -7,10 +7,16 @@ except ImportError:
     from PyQt5.QtCore import Qt, QObject, pyqtSignal
     from PyQt5.QtGui import QIntValidator
 
-# In qt.py
 try:
     from PyQt6 import QtCore
     Key_Return = QtCore.Qt.Key.Key_Return
 except ImportError:
     from PyQt5 import QtCore
     Key_Return = QtCore.Qt.Key_Return
+
+try:
+    from PyQt6 import QtCore, QtGui, QtWidgets
+    from picamera2.previews.qt import QGl6Picamera2 as QGlPicamera2
+except ImportError:
+    from PyQt5 import QtCore, QtGui, QtWidgets
+    from picamera2.previews.qt import QGlPicamera2
